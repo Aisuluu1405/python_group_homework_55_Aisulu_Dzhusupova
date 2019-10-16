20126f48d50f5418551d95e3d72ac96da8f360c5
Айсулу <aisuluueco2009@yandex.ru>
2019-10-16 09:07:31 +0600

    Создала страницу для добавления статьи на базе 'CreateView'.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,20 +1,18 @@
 from django import forms
 from django.forms import widgets
-
 from webapp.models import Article, Comment
 
 
-class ArticleForm(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Title')
-    author = forms.CharField(max_length=40, required=True, label='Author')
-    text = forms.CharField(max_length=3000, required=True, label='Text',
-                           widget=widgets.Textarea)
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        exclude = ['created_at', 'updated_at']
 
 
-class CommentForm(forms.Form):
-    article = forms.ModelChoiceField(queryset=Article.objects.all(), required=False, label='Article')
-    text = forms.CharField(max_length=400, required=True, label='Text', widget=widgets.Textarea)
-    author = forms.CharField(max_length=40, required=False, label='Author')
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['created_at', 'updated_at']
 
 
 class ArticleCommentForm(forms.ModelForm):
--- source/webapp/views/article_views.py
+++ source/webapp/views/article_views.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from webapp.forms import ArticleForm, ArticleCommentForm
 from webapp.models import Article
@@ -38,23 +39,13 @@ class ArticleView(DetailView):
         context['is_paginated'] = page.has_other_pages()
 
 
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article/create.html'
+    form_class = ArticleForm
 
-class ArticleCreateView(View):
-    def get(self, request, *args, **kwargs):
-        form = ArticleForm()
-        return render(request, 'article/create.html', context={'form': form})
-    def post(self, request, *args, **kwargs):
-        form = ArticleForm(data=request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            article = Article.objects.create(
-                title=data['title'],
-                author=data['author'],
-                text=data['text'],
-            )
-            return redirect('article_view', pk=article.pk)
-        else:
-            return render(request, 'article/create.html', context={'form': form})
+    def get_success_url(self):
+        return reverse('article_view', kwargs={'pk': self.object.pk})
 
 
 class ArticleEditView(View):
