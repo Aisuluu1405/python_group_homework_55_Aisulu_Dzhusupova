270fc1053fe5c9839b482c59860ac869441acde0
Айсулу <aisuluueco2009@yandex.ru>
2019-11-02 10:21:01 +0600

    Сделала редактирование пользователя с новыми полями из модели 'Profile', сделала 'Data migration' чтобы можно было редактировать профиль существующих пользователей.

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 from accounts.models import Token
+from .models import Profile
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    fields = ['birth_date', 'avatar']
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [ProfileInLine]
 
 admin.site.register(Token)
+admin.site.unregister(User)
+admin.site.register(User, UserProfileAdmin)
+
 # Register your models here.
